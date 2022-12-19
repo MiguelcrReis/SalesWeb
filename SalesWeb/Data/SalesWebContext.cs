7931c642fcce80e2f358d3c771a6a45dd983fb03
Miguel Reis <miguel.reis@megalaudo.com.br>
2022-12-19 16:31:41 -0300

    Other Entities and second migration

--- SalesWeb/Data/SalesWebContext.cs
+++ SalesWeb/Data/SalesWebContext.cs
@@ -9,11 +9,13 @@ namespace SalesWeb.Data
 {
     public class SalesWebContext : DbContext
     {
-        public SalesWebContext (DbContextOptions<SalesWebContext> options)
+        public SalesWebContext(DbContextOptions<SalesWebContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesWeb.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ SalesWeb/Models/Seller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWeb.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+            Sales.Add(salesRecord);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            Sales.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(salesRecord => salesRecord.Date >= initial && salesRecord.Date <= final).Sum(salesRecord => salesRecord.Amount);
+        }
+    }
+}
