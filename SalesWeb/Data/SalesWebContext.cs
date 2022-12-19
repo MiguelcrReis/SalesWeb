f17990f7466c4853fc6eb0ee458ed738bee0bb3a
Miguel Reis <miguel.reis@megalaudo.com.br>
2022-12-19 11:01:13 -0300

    Created CRUD Departments with  Scaffolding

---
+++ SalesWeb/Data/SalesWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Models;
+
+namespace SalesWeb.Data
+{
+    public class SalesWebContext : DbContext
+    {
+        public SalesWebContext (DbContextOptions<SalesWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWeb.Models.Department> Department { get; set; }
+    }
+}
--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb.Data;
 
 namespace SalesWeb
 {
@@ -33,6 +35,9 @@ namespace SalesWeb
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
