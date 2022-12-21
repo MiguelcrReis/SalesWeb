abbe019e8039dc0c7345f06f965809cf2a232e36
Miguel Reis <miguel.reis@megalaudo.com.br>
2022-12-21 10:14:48 -0300

    Simple search implemented

---
+++ SalesWeb/Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using SalesWeb.Data;
+using SalesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWeb.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebContext _context;
+
+        public SalesRecordService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from sale in _context.SalesRecord select sale;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(X => X.Date)
+                .ToListAsync();
+        }
+    }
+}
--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -48,6 +48,7 @@ namespace SalesWeb
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
