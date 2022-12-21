26bbab38e7de49b2e16b090e03627c30467d2184
Miguel Reis <miguel.reis@megalaudo.com.br>
2022-12-21 10:56:33 -0300

    Simple search implemented Format Date and Amount

--- SalesWeb/Services/SalesRecordService.cs
+++ SalesWeb/Services/SalesRecordService.cs
@@ -37,5 +37,27 @@ namespace SalesWeb.Services
                 .OrderByDescending(X => X.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from sale in _context.SalesRecord select sale;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(X => X.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
